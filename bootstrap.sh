38b21abc471b51f501a323b966e94d2a4b054d61
Gregory Hilkert <EpiphanyMachine@gmail.com>
2014-03-04 16:02:24 -0800

    bootstrap: ignore initial setup files which have to be run manually

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ git pull origin master
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
         --exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "symlinks/" \
-        --exclude ".osx" -av --no-perms . ~
+        --exclude ".osx" --exclude ".cask" --exclude "Brewfile" -av --no-perms . ~
     source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
